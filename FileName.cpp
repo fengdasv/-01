6adcf46e0c06fa7f0c12a7a153779b86c78e76e0
name <1522605509@qq.com>
2025-11-04 15:10:52 +0800

    队列002

---
+++ FileName.cpp
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct vector {
+    int* data;
+    int size;
+} vector;
+
+vector* initvector(int n) {
+    vector* v = (vector*)malloc(sizeof(vector));
+    v->data = (int*)malloc(sizeof(int) * n);
+    v->size = n;
+    return v;
+}
+
+void clearvector(vector* v) {  // 移除void*返回值类型
+    if (v == NULL)return;
+    free(v->data);
+    free(v);
+}
+
+int vectorseek(vector* v, int pos) {
+    if (pos < 0 || pos >= v->size)return -1;
+    return v->data[pos];
+}
+
+int insertvector(vector* v, int pos, int val) {
+    if (pos < 0 || pos >= v->size)return -1;
+    v->data[pos] = val;
+    return 1;
+}
+
+typedef struct Queue {
+    vector* data;
+    int head, tail, count, size;
+} Queue;
+
+Queue* initQueue(int n) {
+    Queue* v = (Queue*)malloc()
+}
+
+int empty(Queue* q) {
+
+}
+
+int push(Queue* q, int val) {
+
+}
+
+int front(Queue* q) {
+
+}
+
+int pop(Queue* q) {
+
+}
+void clearQueue(Queue* q) {
+
+}
+void outputqueue(Queue* q) {  // 修正函数名拼写错误
+
+
+    printf("\n");
+}
+
+int main(int argc, char const* argv[]) {
+    srand(time(0));  // 修正：正确初始化随机数种子
+
+#define MAX_OP 10
+    Queue* q = initQueue(5);
+
+    for (int i = 0; i < MAX_OP; i++) {
+        int op = rand() % 5;
+        int val = rand() % 100;  // 生成随机值
+
+        switch (op) {
+        case 0:
+            if (!empty(q)) {
+                printf("front of queue: %d\n", front(q));
+                pop(q);
+            }
+            else {
+                printf("queue is empty, cannot pop\n");
+            }
+            break;
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+            if (push(q, val)) {
+                printf("push %d to queue\n", val);
+            }
+            else {
+                printf("queue is full, cannot push %d\n", val);
+            }
+            break;
+        }
+        outputqueue(q);
+    }
+
+    clearQueue(q);
+    return 0;
+}
